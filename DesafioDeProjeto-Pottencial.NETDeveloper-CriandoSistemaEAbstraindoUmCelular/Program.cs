f90fba4996bd28c14587dcca4a635a29019de48f
mattyonder <mateussantosor5002@gmail.com>
2023-01-20 23:10:26 -0300

    Commit do Desafio de Projeto

---
+++ DesafioDeProjeto-Pottencial.NETDeveloper-CriandoSistemaEAbstraindoUmCelular/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "11111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone: ");
+Smartphone iphone = new Iphone(numero: "4987", modelo: "Modelo 2", imei: "22222222222", memoria: 128);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
